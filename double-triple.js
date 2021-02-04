04c4851c650ad648f26242284cc0930ac6fe8255
Nazmulhasan97 <md.nazmul.usk97@gmail.com>
2021-02-04 13:17:22 +0600

    double vs triple

---
+++ double-triple.js
@@ -0,0 +1,19 @@
+// Double equal sign==========
+const first = 0;
+const second = false;
+if(first == second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// Triple equal sign ========
+const first = 0;
+const second = false;
+if(first === second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
