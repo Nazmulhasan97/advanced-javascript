de9edad844c40fd0d9de6e9c87304679e44d2d14
Nazmulhasan97 <md.nazmul.usk97@gmail.com>
2021-02-04 12:58:12 +0600

    null vs undefined check

---
+++ null-undefined.js
@@ -0,0 +1,18 @@
+var x;
+
+if (typeof x === "undefined") {
+  txt = "x is undefined";
+} else {
+  txt = "x is defined";
+}
+console.log(x)
+
+typeof null          // "object" (not "null" for legacy reasons)
+typeof undefined     // "undefined"
+null === undefined   // false
+null  == undefined   // true
+null === null        // true
+null == null         // true
+!null                // true
+isNaN(1 + null)      // false
+isNaN(1 + undefined) // true
