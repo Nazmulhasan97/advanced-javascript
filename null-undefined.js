a77d8e73cfb238551021d6bee956e3959f9e64ed
Nazmulhasan97 <md.nazmul.usk97@gmail.com>
2021-02-04 12:59:09 +0600

    some code edit

--- null-undefined.js
+++ null-undefined.js
@@ -7,12 +7,12 @@ if (typeof x === "undefined") {
 }
 console.log(x)
 
-typeof null          // "object" (not "null" for legacy reasons)
-typeof undefined     // "undefined"
-null === undefined   // false
-null  == undefined   // true
-null === null        // true
-null == null         // true
-!null                // true
-isNaN(1 + null)      // false
-isNaN(1 + undefined) // true
+// typeof null          // "object" (not "null" for legacy reasons)
+// typeof undefined     // "undefined"
+// null === undefined   // false
+// null  == undefined   // true
+// null === null        // true
+// null == null         // true
+// !null                // true
+// isNaN(1 + null)      // false
+// isNaN(1 + undefined) // true
