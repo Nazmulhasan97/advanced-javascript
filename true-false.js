b1ea9aa83f8a3152005d0a497e5a8adf78a5ca4c
Nazmulhasan97 <md.nazmul.usk97@gmail.com>
2021-02-04 12:07:38 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+
+// Falsy value => 0 , ""(empty string) , undefined , null , NAN(Not A Number) , false;
+
+
+// Truthy Values => '0' , " " , [] , true;
+
+
+// const age =0;
+const name = true ;
+if(name){
+    console.log('result is true')
+}
+else{
+    console.log('result is false')
+}
